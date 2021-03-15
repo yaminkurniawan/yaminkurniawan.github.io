33f92ca7da833dfcf0a440e5c1ab1772e145df5f
yaminkurniawan <yaminkurniawan31@gmail.com>
2021-03-15 16:20:40 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <title>Kembara</title>
+  </head>
+  <body id="page-top">
+   
+   <!-- navbar -->
+   <nav class="navbar navbar-expand-lg navbar-dark bg-transparent fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="img/logo.png" alt="" width="20" class="d-inline-block alight-top">
+             KEMBARA
+          </a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-tuggler" aria-current="page" href="#Peraturan Organisasi">Peraturan Organisasi</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-tuggler" href="#Struktur">Struktur</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-tuggler" href="#Data Base">Data Base</a>
+            </li>
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#Media Sosial" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Media Sosial
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <li><a class="dropdown-item" href="https://www.instagram.com/kembara.official/">Instagram</a></li>
+              <li><a class="dropdown-item" href="https://www.facebook.com/kembara.official">Facebook</a></li>
+              <li><a class="dropdown-item" href="https://www.youtube.com/channel/UCjxVHVMN6xSFKVizHXRaLPg">Youtube</a></li>
+              <li><a class="dropdown-item" href="http://www.kembaraofficial.com/">Website</a></li>
+            </ul>
+          </ul>
+        </div>
+      </div>
+    </nav>
+<!-- navbar -->
+
+
+<!-- slide -->
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="img/slide1.jpg" class="d-block w-100" alt="slide1">
+       <div class="carousel-caption d-none d-md-block">
+        <h1>KEMBARA</h1>
+        <h2 class="lead">Keluarga Mahasiswa Kabupaten Bandung Barat</h2>
+        <br>
+        <button type="button" class="btn btn-light">Kunjungi</button>
+        <br> <br> <br>
+      </div>
+        </div>
+        <div class="carousel-item">
+          <img src="img/slide2.jpg" class="d-block w-100" alt="slide2">
+        </div>
+        <div class="carousel-item">
+          <img src="img/slide3.jpg" class="d-block w-100" alt="slide3">
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"  data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"  data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+<!-- slide -->
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+  </body>
+</html>\
